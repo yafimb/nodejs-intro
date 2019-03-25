a9fa47b6c63558d52469e3f2a1a08567b7495c4b
ZENBOOK\fimab <yafimb@zoara.com>
2019-03-25 17:35:28 +0200

    Sending response

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req,res);
+    console.log(req.url,req.method, req.headers);
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<h1>Hello from nodejs</h1>');
+    res.write(`<h2>Route ${req.url}</h2>`);
+    res.write('</html>');
+    res.end();
+    //process.exit();
+    
 });
 
 server.listen(3000);
