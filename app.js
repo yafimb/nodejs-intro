7d1e92059dec1d009f67450d2147f8cea2d86603
ZENBOOK\fimab <yafimb@zoara.com>
2019-03-28 14:29:47 +0200

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
+    const url = req.url;
+
+    if(url === '/')
+    {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<h1>Hello from nodejs</h1>');
+        res.write(`<h2>Route ${req.url}</h2>`);
+        res.write('</html>');
+        res.end();    
+    }
+
     console.log(req.url,req.method, req.headers);
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<h1>Hello from nodejs</h1>');
-    res.write(`<h2>Route ${req.url}</h2>`);
-    res.write('</html>');
-    res.end();
+    
     //process.exit();
     
 });
